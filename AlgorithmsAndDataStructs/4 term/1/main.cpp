3d4403b3e0b534b27c95bc4ae9bd19d078a40b3a
Jeavie <dejeaview@gmail.com>
2018-02-09 20:30:44 +0300

    Upd

--- AlgorithmsAndDataStructs/4 term/1/main.cpp
+++ AlgorithmsAndDataStructs/4 term/1/main.cpp
@@ -1,7 +1,120 @@
-#include <iostream>
+//#include <iostream>
 //#include "conio.h"
-#include <ctime>
-#include "shape.h"
+//#include <ctime>
+//#include "shape.h"
+//#include "screen.h"
+//
+//class h_circle : public rectangle, public reflectable{
+//    bool reflected;
+//public:
+//    h_circle(point a, point b, bool r = true) : rectangle (a, b), reflected(r) {}
+//    void draw();
+//    void flip_horisontally() {};
+//    void flip_vertically() { reflected = !reflected; };
+//};
+// void h_circle ::draw() {
+//     int x0 = (sw.x + ne.x) / 2;
+//     int y0 = reflected ? sw.y : ne.y;
+//     int radius = (ne.x - sw.x) / 2;
+//     int x = 0;
+//     int y = radius;
+//     int delta = 2 - 2 * radius;
+//     int error = 0;
+//     while (y >= 0){
+//         if (reflected) {
+//             put_point(x0 + x, y0 + 0.7 * y);
+//             put_point(x0 - x, y0 + 0.7 * y);
+//         } else {
+//             put_point(x0 + x, y0 - 0.7 * y);
+//             put_point(x0 - x, y0 - 0.7 * y);
+//         }
+//         error = 2 * (delta + y) - 1;
+//         if (delta < 0 && error <= 0) {++x; delta += 2 * x + 1;
+//             continue;
+//         }
+//         error = 2 * (delta - x) - 1;
+//         if (delta > 0 && error > 0) {--y; delta += 1 - 2 * y;
+//             continue;
+//         }
+//         ++x; delta += 2 * (x - y); --y;
+//     }
+// }
+//
+//void down(shape& p, shape& q){
+//    point n = q.south();
+//    point s = p.north();
+//    p.move(n.x - s.x, n.y - s.y - 1);
+//}
+//
+//class myshape : public rectangle{
+//    int w, h;
+//    line l_eye;
+//    line r_eye;
+//    line mouth;
+//public:
+//    myshape(point, point);
+//    void draw();
+//    void move(int, int);
+//    void resize(int) {}
+//};
+//
+//
+//
+//myshape ::myshape(point a, point b) : rectangle (a, b),
+//                                      w(neast().x - swest().x + 1),
+//                                      h(neast().y - swest().y + 1),
+//                                      l_eye(point(swest().x + 2, swest().y + h * 3 / 4), 2),
+//                                      r_eye(point(swest().x + w - 4, swest().y + h * 3 / 4), 2),
+//                                      mouth(point(swest().x + 2, swest().y + h / 4), w - 4) {}
+//
+//void myshape :: draw() {
+//    rectangle :: draw();
+//    int a = (swest().x + neast().x) / 2;
+//    int b = (swest().y + neast().y) / 2;
+//    put_point(point(a, b));
+//}
+//
+//void myshape ::move(int a, int b) {
+//    rectangle :: move(a, b);
+//    l_eye.move(a, b);
+//    r_eye.move(a, b);
+//    mouth.move(a ,b);
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "RUS");
+//    //Инициализаия фигур
+//    screen_init();
+//    rectangle p1(point(0, 0), point(12, 5));
+//    line p2(point(0, 15), 17);
+//    myshape p3(point(15, 10), point(27, 18));
+//    h_circle p4(point(40, 10), point(50, 20));
+//    shape_refresh();
+//    std :: cout << "GENERATED \n";
+//    std :: cin.get(); //смотреть исходный набор
+//    p1.rotate_right();
+//    p2.resize(2);
+//    p4.flip_vertically();
+//    shape_refresh();
+//    std :: cout << "PREPARED \n";
+//    std :: cin.get();
+//    //сборка изображения
+//    p3.move(10, 0);
+//    up(p2, p3);
+//    up(p1, p2);
+//    down(p4, p3);
+//    shape_refresh();
+//    std :: cout << "READY \n";
+//    std :: cin.get();
+//    screen_destroy();
+//    return 0;
+//}
+//_________________________________________________________________________________________________________________________
+//Был частично использован код Bjarne Stroustrup'а
+#include <iostream>
+#include "time.h"
+#include "sh.h"
+#include "string.h"
 
 using namespace std;
 
--- AlgorithmsAndDataStructs/4 term/1/shape.h
+++ AlgorithmsAndDataStructs/4 term/1/shape.h
@@ -1,14 +1,69 @@
-////
-//// Created by jeavie on 07.02.2018.
-////
+//
+// Created by jeavie on 09.02.2018.
+//
 //
 //#pragma once
 //#include <iostream>
 //#include <string>
 //#include "screen.h"
-////Базовый класс фигур
+//
+//поддержка экрана в форме матрицы символов
+//
+//char screen[XMAX][YMAX];
+//
+//enum color { black = '*', white = '.' };
+//
+//void screen_init() {
+//    for (auto y = 0; y < YMAX; ++y)
+//        for (auto &x : screen[y])
+//            x = white;
+//}
+//
+//void screen_destroy() {
+//    for (auto y = 0; y < YMAX; ++y)
+//        for (auto &x : screen[y])
+//            x = black;
+//}
+//
+//bool on_screen(int a, int b) { //проверка попадания на экран
+//    return 0 <= a && a < XMAX && 0 <= b && b < YMAX;
+//}
+//
+//void put_line(int x0, int y0, int x1, int y1) { // рисование отрезка по пряямой по алгоритму Брезенхэма
+//    int dx = 1;
+//    int a = x1 - x0;
+//    if (a < 0) dx = -1, a = -a;
+//    int dy = 1;
+//    int b = y1 - y0;
+//    if (b < 0) dy = -1, b = -b;
+//    int two_a = 2 * a;
+//    int two_b = 2 * b;
+//    int xcrit = -b + two_a;
+//    int eps = 0;
+//
+//    for (;;) { // формирование прямой линии по точкам
+//        put_point(x0, y0);
+//        if (x0 == x1 && y0 == y1) break;
+//        if (eps <= xcrit) x0 += dx, eps += two_b;
+//        if (eps >= a || a < b) y0 += dy, eps -= two_a;
+//    }
+//}
+//
+//void screen_clear() { // очистка экрана
+//    screen_init();
+//}
+//
+//void screen_refresh() {
+//    for (int y = YMAX - 1; 0 <= y; --y) { // с верхней строки до нижней
+//        for (auto x : screen[y]) // от левого столбца до правого
+//            std :: cout << x;
+//        std :: cout << '\n';
+//    }
+//}
+//
+//2. Библиотека фигур
 //struct shape {
-//    static shape* list;
+//    static shape* list; // начало списка фигур (одно на все фигуры)
 //    shape* next;
 //    shape() { next = list; list = this; }
 //    virtual point north() const = 0;
@@ -22,144 +77,42 @@
 //    virtual void draw() = 0;
 //    virtual void move(int, int) = 0;
 //    virtual void resize(int) = 0;
-//
-//private:
-//    shape(const shape&);
-//    shape(const shape&&);
 //};
 //
-////--------------------------------------------------------------------
-////Работа с экраном
+//shape * shape :: list = nullptr; // инициализация списка фигур
 //
-//char screen[XMAX][YMAX];
-//
-//enum color { black = '*', white = ' ' };
-//
-//void screen_init()
-//{
-//    for (int y = 0; y < YMAX; ++y)
-//        for (int x = 0; x < XMAX; ++x)
-//            screen[x][y] = white;
-//}
-//
-//inline int on_screen(int a, int b) //Попадание на экран
-//{
-//    return (0 <= a && a < XMAX && 0 <= b && b < YMAX);
-//}
-//
-//void put_point(int a, int b)
-//{
-//    if (on_screen(a, b)) screen[a][b] = black;
-//}
-//
-//void put_line(int x0, int y0, int x1, int y1)
-//{
-//    /*
-//    Рисование отрезка прямой (x0, y0) - (x1, y1)
-//    Уравнение прямой: b(x-x0) + a(y-y0) = 0
-//    Минимизируется величина abs(eps)
-//    где eps = 2*(b(x-x0))+a(y-y0)
-//    */
-//    int dx = 1;
-//    int a = x1 - x0;
-//    if (a < 0) dx = -1, a = -a;
-//    int dy = 1;
-//    int b = y1 - y0;
-//    if (b < 0) dy = -1, b = -b;
-//    int two_a = 2 * a;
-//    int two_b = 2 * b;
-//    int xcrit = -b + two_a;
-//    register int eps = 0;
-//
-//    while (1) {
-//        put_point(x0, y0);
-//        if (x0 == x1 && y0 == y1) break;
-//        if (eps <= xcrit) x0 += dx, eps += two_b;
-//        if (eps >= a || a < b)y0 += dy, eps -= two_a;
-//    }
-//}
-//
-//void screen_clear() { screen_init(); }
-//
-//void screen_refresh()
-//{
-//    for (int y = YMAX-1; 0<=y; --y) {
-//        for (int x = 0; x < XMAX; ++x)
-//            std::cout << screen[x][y];
-//        //std::cout << '\n';
-//    }
-//}
-//
-//
-//shape* shape::list = nullptr; //Иницализация списка фигур
-//
-//class rotatble : public virtual shape { //Фигуры, пригодные к повороту
+//class rotatable : virtual public shape { // фигуры, пригодные к повороту
 //public:
 //    virtual void rotate_left() = 0;
 //    virtual void rotate_right() = 0;
 //};
 //
-//class reflectable : public virtual shape {//Фигуры пригодные к зеркальному отражению
+//class reflectable : virtual  public shape { // фигуры, пригодные к зеркальному отражению
 //public:
-//    virtual void flip_horisontally() = 0;
+//    virtual void flip_horisontallly() = 0;
 //    virtual void flip_vertically() = 0;
 //};
 //
-//class line : public shape {
-//    /*
-//    Отрезок прямой ["w", "e"]
-//    north() определяет точку "выше центра отрезка и так далеко на север,
-//    как самая его северная точка", и т.п.
-//    */
+//class line : public shape{
 //protected:
 //    point w, e;
-////    line(const line&);
-////    line(const line&&);
 //public:
-//    /*Как было
-//    point north() const { return point((w.x + e.x) / 2, e.y<w.y ? w.y : e.y); }
-//    point south() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point east() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point west() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point neast() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point seast() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point nwest() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    point swest() const { return point((w.x + e.x) / 2, e.y<w.y ? e.y : w.y); }
-//    */
-//    /*Исправление предыдущего кода
-//    point north() const { return point((w.x + e.x) / 2, e.y < w.y ? w.y : e.y); }
-//    point south() const { return point((w.x + e.x) / 2, e.y < w.y ? e.y : w.y); }
-//    point east() const { return point(e.x, (w.y + e.y) / 2); }
-//    point west() const { return point(w.x, (w.y + e.y) / 2); }
-//    point neast() const { return point(e.x, e.y < w.y ? w.y : e.y); }
-//    point seast() const { return point(e.x, e.y < w.y ? e.y : w.y); }
-//    point nwest() const { return point(w.x, e.y < w.y ? w.y : e.y); }
-//    point swest() const { return point(w.x, e.y < w.y ? e.y : w.y); }
-//    */
-//    //Более рациональное исправление
-//    point north() const { return point((w.x + e.x) / 2, e.y < w.y ? w.y : e.y); }
-//    point south() const { return point((w.x + e.x) / 2, e.y < w.y ? e.y : w.y); }
-//    point east() const { return e; }
-//    point west() const { return w; }
-//    point neast() const { return e; }
-//    point seast() const { return e; }
-//    point nwest() const { return w; }
-//    point swest() const { return w; }
-//    void move(int a, int b) { w.x += a; w.y += b;e.x += a; e.y += b; }
-//
-//    void draw() {
-//        put_line(w, e);
-//    }
-//
-//    line(point a, point b) :w(a), e(b) {
-//    }
-//
-//    line(point a, int l) :w(a), e(point(a.x + l - 1, a.y)) {
-//    }
+//    line (point a, point b) : w(a), e(b) { };
+//    line (point a, int L) : w(point(a.x + L - 1, a.y)), e(a) { };
+//    point north() const { return point((w.x + e.x)/2, e.y < w.y ? w.y : e.y); }
+//    point south() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point east() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point west() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point neast() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point seast() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point nwest() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    point swest() const { return point((w.x + e.x)/2, e.y < w.y ? e.y : w.y); }
+//    void move(int a, int b) {w.x += a; w.y +=b; e.x += a; e.y += b; }
+//    void draw() { put_line(w, e); }
+//    void resize (int d) { e.x += (e.x - w.x) * (d - 1); e.y += (e.y - w.y) * (d - 1); } // увеличение в (d) раз
 //};
 //
-////Прямоугольник
-//class rectangle:public rotatble {
+//class rectangle : public rotatable { // прямоугольник
 //    /*
 //    nw-----n-----ne
 //    |             |
@@ -167,28 +120,25 @@
 //    w      c      e
 //    |             |
 //    |             |
-//    sw-----s-----se
-//    */
-//    rectangle(const rectangle&);
-//    rectangle(const rectangle&&);
+//    sw-----s-----se */
+//protected:
 //    point sw, ne;
 //public:
+//    rectangle (point, point);
 //    point north() const { return point((sw.x + ne.x) / 2, ne.y); }
 //    point south() const { return point((sw.x + ne.x) / 2, sw.y); }
-//    point east() const { return point(ne.x, (sw.y + ne.y) / 2); }
-//    point west() const { return point(sw.x, (sw.y + ne.y) / 2); }
+//    point east() const { return point(sw.x, (sw.y + ne.y) / 2); }
+//    point west() const { return point(ne.x, (sw.y + ne.y) / 2); }
 //    point neast() const { return ne; }
-//    point seast() const { return point(ne.x, sw.y); }
-//    point nwest() const { return point(sw.x, ne.y); }
+//    point seast() const { return point(sw.x, ne.y); }
+//    point nwest() const { return point(ne.x, sw.y); }
 //    point swest() const { return sw; }
-//    void rotate_right()//Поворот относительно se
-//    {
+//    void rotate_right() { //Поворот относительно se
 //        int w = ne.x - sw.x, h = ne.y - sw.y;
 //        sw.x = ne.x - h * 2;
 //        ne.y = sw.y + w / 2;
 //    }
-//    void rotate_left() //Поворот относительно sw
-//    {
+//    void rotate_left() { //Поворот относительно sw
 //        int w = ne.x - sw.x, h = ne.y - sw.y;
 //        ne.x = sw.x + h * 2;
 //        ne.y = sw.y + w / 2;
@@ -200,177 +150,39 @@
 //        ne.x += a;
 //        ne.y += b;
 //    }
-//    void draw();
-//    rectangle(point, point);
-//};
-//
-//rectangle::rectangle(point a, point b)
-//{
-//    if(a.x<=b.x)
-//        if (a.y <= b.y) {
-//            sw = a;
-//            ne = b;
-//        }
-//        else {
-//            sw = point(a.x, b.y);
-//            ne = point(b.x, a.y);
-//        }
-//    else
-//    if (a.y <= b.y) {
-//        sw = point(b.x, a.y);
-//        ne = point(a.x, b.y);
+//    void resize (int d){
+//        ne.x += (ne.x - sw.x) * (d - 1);
+//        ne.y += (ne.y - sw.y) * (d - 1);
 //    }
-//    else {
-//        sw = b;
-//        ne = a;
-//    }
-//}
-//void rectangle::draw()
-//{
-//    point nw(sw.x, ne.y);
-//    point se(ne.x, sw.y);
-//    put_line(nw, ne);
-//    put_line(ne, se);
-//    put_line(se, sw);
-//    put_line(sw, nw);
-//}
-//
-////Треугольник
-//class Triangle:public rotatble, public reflectable {
-//    /*
-//    nw    top   ne
-//          / \
-//         /   \
-//        /     \
-//    w  /   c   \  e
-//      /         \
-//     /           \
-//    sw-----s-----se
-//    */
-//    Triangle(const Triangle&);
-//    Triangle(const Triangle&&);
-//    point n, sw, se;
-//    int pos;
-//public:
-//    Triangle(point, point, point);
-//    point north() const { return n; }
-//    point south() const { return point((sw.x + se.x) / 2, sw.y); }
-//    point east() const { return point(se.x, (se.y + n.y) / 2); }
-//    point west() const { return point(sw.x, (sw.y + n.y) / 2); }
-//    point neast() const { return point(se.x, n.y);}
-//    point seast() const { return se; }
-//    point nwest() const { return point(sw.x, n.y);}
-//    point swest() const { return sw; }
-//
-//    void rotate_right()
-//    {
-//        pos = (pos + 1) % 4;
-//    }
-//
-//    void rotate_left()
-//    {
-//        if (pos)
-//            pos -= 1;
-//        else
-//            pos = 3;
-//    }
-//
-//    void flip_horisontally() {
-//        if (pos == 0)
-//            pos = 2;
-//        else
-//        if (pos == 2)
-//            pos = 0;
-//    }
-//    void flip_vertically() {
-//        if (pos == 1)
-//            pos = 3;
-//        else
-//        if (pos == 3)
-//            pos = 1;
-//    }
-//
-//    void move(int a, int b){
-//        sw.x += a;
-//        sw.y += b;
-//        se.x += a;
-//        se.y += b;
-//        n.x += a;
-//        n.y += b;
-//    }
-//
 //    void draw();
 //};
 //
-//Triangle::Triangle(point lft, point rgt, point up)
-//{
-//    sw = lft;
-//    se = rgt;
-//    n = up;
-//    pos = 0;
-//}
-//
-//void Triangle::draw()
-//{
-//    point curTop, curBaseOne, curBaseTwo; //Текущее положение
-//    switch (pos) {
-//        case 0:
-//            curBaseOne = sw;
-//            curBaseTwo = se;
-//            curTop = n;
-//            break;
-//        case 1:
-//            curBaseOne = nwest();
-//            curBaseTwo = sw;
-//            curTop = east();
-//            break;
-//        case 2:
-//            curBaseOne = nwest();
-//            curBaseTwo = neast();
-//            curTop = south();
-//            break;
-//        case 3:
-//            curBaseOne = neast();
-//            curBaseTwo = se;
-//            curTop = west();
-//            break;
+//rectangle ::rectangle(point a, point b) {
+//    if (a.x <= b.x){
+//        if (a.y <= b.y) sw = a, ne = b;
+//        else sw = point (a.x, b.y), ne = point (b.x, a.y);
+//    } else {
+//        if (a.y <= b.y) sw = point(b.x, a.y), ne = point(a.x, b.y);
+//        else sw = b, ne = a;
 //    }
-//    put_line(curBaseOne, curBaseTwo);
-//    put_line(curBaseOne, curTop);
-//    put_line(curBaseTwo, curTop);
 //}
 //
-//void shape_refresh()//Перерисовка всех фигур
-//{
+//void rectangle :: draw() {
+//    put_line(nwest(), ne);
+//    put_line(ne, seast());
+//    put_line(seast(), sw);
+//    put_line(sw, nwest());
+//}
+//
+//void shape_refresh() { // перерисовкка всех фигур
 //    screen_clear();
-//    for (shape* p = shape::list; p; p = p->next) p->draw();
+//    for (shape* p = shape :: list; p; p = p -> next) p -> draw();
 //    screen_refresh();
 //}
 //
-//void up(shape* p, const shape* q)//Поместить p над q
-//{
-//    point n = q->north();
-//    point s = p->south();
-//    p->move(n.x - s.x, n.y - s.y + 1);
+//void up(shape& p, const shape& q) { // поместить p над q, обычная функция, а не член класса
+//    point n = q.north();
+//    point s = p.south();
+//    p.move(n.x - s.x, n.y - s.y + 1);
 //}
-//
-//void lftUp(shape* p, const shape* q)//Поместить p слева над q
-//{
-//    p->move(q->nwest().x - p->swest().x, q->nwest().y - p->swest().y + 1);
-//}
-//
-//void rgtUp(shape* p, const shape* q)//Поместить p справа над q
-//{
-//    p->move(q->neast().x - p->seast().x, q->nwest().y - p->swest().y + 1);
-//}
-//
-//void rgtDwn(shape* p, const shape* q)//Поместить p справа под q
-//{
-//    p->move(q->east().x - p->west().x, q->swest().y - p->nwest().y);
-//}
-//
-//void lftDwn(shape* p, const shape* q)//Поместить p справа под q
-//{
-//    p->move(q->west().x - p->east().x, q->swest().y - p->nwest().y);
-//}
-//
+
